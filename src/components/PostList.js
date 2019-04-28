904714bda8ded7e7cf848019a85c16db443ffbfa
Bill Pliske <bpliske@gmail.com>
2019-04-27 20:04:48 -0700

    got latest posts to show images

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,52 +1,65 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
 
 export default class IndexPage extends React.Component {
   render() {
     const { posts, title } = this.props
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">{title}</h1>
+      <HomeWrapper>
+        <div>
+          <div>
+            <h1>{title}</h1>
           </div>
-          {posts.map(({ node: post }) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
-              <p>
-                <Link className="has-text-primary" to={`/blog/${post.slug}`}>
-                  {post.title}
-                </Link>
-                <span> &bull; </span>
-                <small>
-                  {post.date}
-                  {' '}
-- posted by
-                  {' '}
-                  <Link to={`/author/${post.author.slug}`}>
-                    {post.author.name}
+          <PostsList>
+            {posts.map(({ node: post }) => (
+              <HomeListItem key={post.id}>
+                {post.featured_media && (
+                  <Link to={`/blog/${post.slug}`}>
+                    <ImageOuterWrapper>
+                      <Image
+                        src={
+                          post.featured_media.localFile.childImageSharp.sizes
+                            .src
+                        }
+                        sizes={
+                          post.featured_media.localFile.childImageSharp.sizes
+                        }
+                        alt="blog_image_one"
+                      />
+                    </ImageOuterWrapper>
                   </Link>
-                </small>
-              </p>
-              <div>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: post.excerpt.replace(/<p class="link-more.*/, ''),
-                  }}
-                />
-                <Link className="button is-small" to={`/blog/${post.slug}`}>
-                  Keep Reading →
-                </Link>
-              </div>
-            </div>
-          ))}
+                )}
+
+                <p>
+                  {post.title}
+                  <span> &bull; </span>
+                  <small>
+                    {post.date}
+                    {' '}
+- posted by
+                    {' '}
+                    <Link to={`/author/${post.author.slug}`}>
+                      {post.author.name}
+                    </Link>
+                  </small>
+                </p>
+                <div>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: post.excerpt.replace(/<p class="link-more.*/, ''),
+                    }}
+                  />
+                  <Link to={`/blog/${post.slug}`}>Keep Reading →</Link>
+                </div>
+              </HomeListItem>
+            ))}
+          </PostsList>
         </div>
-      </section>
+      </HomeWrapper>
     )
   }
 }
@@ -68,7 +81,57 @@ export const pageQuery = graphql`
         wordpress_48
       }
     }
+    featured_media {
+      localFile {
+        childImageSharp {
+          id
+          sizes(maxWidth: 1000) {
+            ...GatsbyImageSharpSizes
+          }
+        }
+      }
+    }
     date(formatString: "MMMM DD, YYYY")
     slug
   }
+`
+
+const HomeWrapper = styled.section`
+  margin: 20px;
+`
+
+const PostsList = styled.ul`
+  list-style: none;
+  margin: 0px auto;
+  padding: calc(30px) 0px calc(10px);
+`
+
+const HomeListItem = styled.li`
+  margin: 20px 0;
+  border: 1px solid transparent;
+  border-radius: 10px;
+  margin: calc(20px * 2) 0 calc(20px * 3);
+  padding: 10px;
+  position: relative;
+  transition: all 0.5s;
+  background: transparent;
+`
+const ImageOuterWrapper = styled.div`
+  border-radius: 10px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(236, 235, 234);
+  border-image: initial;
+  overflow: hidden;
+  img {
+    z-index: -1;
+    transition: opacity 0.5s ease 0.5s;
+  }
+  div div {
+    padding-bottom: 50% !important;
+  }
+`
+
+const Image = styled(Img)`
+  transition: opacity 0.5s ease 0.5s;
 `
